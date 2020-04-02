9c14757c990a5c2c97c1ed7d24a3b572abba8767
Jordan Coughenour <jcoughenour18@gmail.com>
2020-04-02 11:23:05 -0400

    trying to add my search option. Need to figure out how to call the search

---
+++ src/Components/search/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+function Search(props) {
+  const inputHandler = event => {
+    props.search(event.target.value);
+  };
+  return (
+    <div className="navbar navbar-light bg-light">
+      <form className="form-inline">
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          value={props.theTerm}
+          onChange={inputHandler}
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+export default Search;
